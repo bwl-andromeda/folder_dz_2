9ecc6b7aee98a6fe8a4006fc2e489e8242101e35
Nikita <torchhik_darkness@mail.ru>
2023-02-26 14:40:05 +0300

    Final Version_V3

--- first_work.py
+++ first_work.py
@@ -1,22 +1,27 @@
 from pprint import pprint
-with open('recipe.txt', encoding="UTF-8") as file:
-    cook_book = {}
-    for line in file:
-        dish_names = line.strip()
-        count_ingridients = int(file.readline())
-        ingridients_1 = []
-        for _ in range(count_ingridients):
-            Ingredients = file.readline().strip()
-            ingredient_name,quantity,measure = Ingredients.split(" | ")
-            ingridients_1.append(
-                {"ingredient_name": ingredient_name ,"quantity": quantity , "measure": measure}
-                                  )
-        cook_book[dish_names] = ingridients_1
-        file.readline()
+import time
+def open_file():
+    with open('recipe.txt', encoding="UTF-8") as file:
+        cook_book = {}
+        for line in file:
+            dish_names = line.strip()
+            count_ingridients = int(file.readline())
+            ingridients_1 = []
+            for _ in range(count_ingridients):
+                Ingredients = file.readline().strip()
+                ingredient_name,quantity,measure = Ingredients.split(" | ")
+                ingridients_1.append(
+                    {"ingredient_name": ingredient_name ,"quantity": quantity , "measure": measure}
+                                    )
+            cook_book[dish_names] = ingridients_1
+            file.readline()
+    return cook_book
 
 # pprint(cook_book,width=100)
 
 def get_shop_list_by_dishes(dishes, person_count):
+    start = time.time()
+    cook_book = open_file()
     list_1,list_2,list_3 = [],[],[] 
     for i in cook_book.keys():
         for j in dishes:
@@ -42,7 +47,10 @@ def get_shop_list_by_dishes(dishes, person_count):
             if a == c:
                 b["quantity"]+=d["quantity"]
     pprint(basic_dict)
+    end = time.time() - start
+    print()
+    print(end)
 
 
-
+open_file()
 get_shop_list_by_dishes(["Омлет","Жаренная картошка","Запеченный картофель"],2)
